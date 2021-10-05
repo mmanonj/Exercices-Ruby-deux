3e3bd134cf6d50292c3f5ed115bb477cc13bee0d
Manon <manonjanin1@gmail.com>
2021-10-05 16:08:05 +0800

    exos 6 à 11

---
+++ Exo_06.rb
@@ -0,0 +1,9 @@
+
+puts "Bonjour, peux-tu me donner un nombre, s'il-te-plaît ?"
+number = gets.chomp
+
+number_int = number.to_i-1
+
+number_int.times do
+    puts "Salut ça farte ?"
+end
---
+++ Exo_11.rb
@@ -0,0 +1,14 @@
+puts "En quelle année es tu né ?"
+print "> "
+year = gets.chomp
+
+year_int =year.to_i
+
+
+
+"Il y a #{2021-y} ans, tu avais #{y-year_int} ans".
+
+
+year_int.times do |i|
+    puts i
+   end 
